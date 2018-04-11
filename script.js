1fb3a2769723c5629fe5bfed8cd016a741b2a3d2
dejijaye <dejijaye@gmail.com>
2018-04-11 12:34:31 +0100

    switch to es6

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var addBtns = document.querySelectorAll('.btn-add');
-var removeBtns = document.querySelectorAll('.remove');
+let addBtns = document.querySelectorAll('.btn-add');
+let removeBtns = document.querySelectorAll('.remove');
 
 // Shopping cart initialized with some items in it.
-var shoppingCart = [{
+let shoppingCart = [{
     id: "4",
     inCart: "true",
     name: "ASSASSIN'S CREED: DIRECTOR'S CUT",
@@ -16,7 +16,7 @@ var shoppingCart = [{
     price: "5.99"
 }];
 
-function addToCart(event) {
+let addToCart = (event) => {
     event.preventDefault();
     var productDetail = this.dataset
     if(productDetail.inCart === 'false') {
@@ -30,7 +30,7 @@ function addToCart(event) {
     updateCart();
 }
 
-function removeFromCart(event) {
+let removeFromCart = (event) => {
     event.preventDefault();
     var self = this;
     var indexToDelete;
@@ -49,27 +49,61 @@ function removeFromCart(event) {
 }
 
 // register event handlers
-for(var i = 0; i < addBtns.length; i++) {
-    addBtns[i].addEventListener('click', addToCart);
-}
 
-for(var i = 0; i < removeBtns.length; i++) {
-    removeBtns[i].addEventListener('click', removeFromCart);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    for(let i = 0; i < addBtns.length; i++) {
+        addBtns[i].addEventListener('click', (event) => {
+            event.preventDefault();
+            let productDetail = this.dataset
+            if(productDetail.inCart === 'false') {
+                shoppingCart.push(productDetail);
+                this.dataset.inCart = 'true';
+                this.textContent = 'IN CART';
+                this.classList.add('btn-disable');
+                this.previousElementSibling.classList.add('hide'); 
+            }
+            
+            updateCart();
+        });
+    }
+    
+    for(let i = 0; i < removeBtns.length; i++) {
+        removeBtns[i].addEventListener('click', (event) => {
+            event.preventDefault();
+            var self = this;
+            var indexToDelete;
+            var itemToDelete
+            shoppingCart.forEach(function(item) {
+                if(self.dataset.id === item.id) {
+                    indexToDelete = shoppingCart.indexOf(item);
+                    itemToDelete = item
+                }
+            });
+        
+            shoppingCart.splice(indexToDelete, 1);
+            updateCart();
+            resetButton(itemToDelete.id);
+        
+        });
+    }
+
+    document.querySelector('.clear-cart').addEventListener('click', clearCart);
+
+}, false);
 
 
-function updateCart() {
+let updateCart = () => {
 
     if(shoppingCart.length > 0) {
-        var totalAmount = shoppingCart.reduce(function (a,b) { return a + Number(b.price); }, 0);
+        let totalAmount = shoppingCart.reduce((a,b) =>  a + Number(b.price), 0);
     
-        var totalItems = shoppingCart.length;
+        let totalItems = shoppingCart.length;
         document.querySelector('.total-item').innerHTML = totalItems;
-        var holderHTML = '<div class="product cart-info"><span class="cart-summary">' + formatText(totalItems) + 
+        let holderHTML = '<div class="product cart-info"><span class="cart-summary">' + formatText(totalItems) + 
                         '<div class="cart-right" style="float: right;"><span class="cart-total">$' + totalAmount + 
                         '</span><button class="btn btn-clear clear-cart">CLEAR CART</button></div></div>';
         
-        shoppingCart.forEach(function(product) {
+        shoppingCart.forEach(product => {
 
             holderHTML += '<div class="product"><img src="'+ "img/img" + product.id + 
                     '.png" alt=""><span class="product-title">' + product.name + 
@@ -80,15 +114,15 @@ function updateCart() {
         document.querySelector('.clear-cart').addEventListener('click', clearCart);
 
         // register event handler on newly created buttons
-        for(var i = 0; i < document.querySelectorAll('.remove').length ; i++) {
+        for(let i = 0; i < document.querySelectorAll('.remove').length ; i++) {
             document.querySelectorAll('.remove')[i].addEventListener('click', removeFromCart);
         }
 
     } else {
-        var totalAmount = 0;
-        var totalItems = shoppingCart.length;
+        let totalAmount = 0;
+        let totalItems = shoppingCart.length;
         document.querySelector('.total-item').innerHTML = totalItems;
-        var holderHTML = '<div class="product cart-info"><span class="cart-summary">' + formatText(totalItems) + '</span></div>';
+        let holderHTML = '<div class="product cart-info"><span class="cart-summary">' + formatText(totalItems) + '</span></div>';
         
         document.querySelector('.dropdown-content').innerHTML = holderHTML;
         document.querySelector('.cart-summary').style.top = 0;
@@ -96,18 +130,18 @@ function updateCart() {
 
 }
 
-function formatText(n) {
+let formatText = (n) => {
     return  `${n} ITEM(S) IN CART`; 
 }
 
-function clearCart() {
+let clearCart = () => {
     shoppingCart = [];
     updateCart();
     resetButtons();
 }
 
-function resetButtons() {
-    for(var i = 0; i < addBtns.length; i++) {
+let resetButtons = () => {
+    for(let i = 0; i < addBtns.length; i++) {
         if(addBtns[i].dataset.owned !== "true") {
             addBtns[i].classList.remove('btn-disable');
             addBtns[i].previousElementSibling.classList.remove('hide');
@@ -117,12 +151,11 @@ function resetButtons() {
     }
 }
 
-function resetButton(id) {
-    var button = addBtns[id];
+let resetButton = (id) => {
+    let button = addBtns[id];
     button.dataset.inCart = "false";
     button.textContent = "$" + button.dataset.price;
     button.classList.remove('btn-disable');
     button.previousElementSibling.classList.remove('hide');
 }
 
-document.querySelector('.clear-cart').addEventListener('click', clearCart);
